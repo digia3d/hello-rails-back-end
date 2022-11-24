0328a8dc3afe5fab76ca00a33b0f3d3c0814f49b
Aleksandra Ujvari <ujvari65@gmail.com>
2022-11-24 17:35:58 +0100

    Config routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :greetings
+      resources :greetings, only: [:index]
 
       # root "articles#index"
     end
